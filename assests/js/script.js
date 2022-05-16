767087e8c650b204ad4e5a22e421f737c7cafabe
sdmnny <sdmnny@gmail.com>
2022-05-16 12:20:12 -0400

    created inital functions on script file

---
+++ assests/js/script.js
@@ -0,0 +1,46 @@
+// (Dependencies)
+// - need start button
+var startbutton = document.querySelector("#start");
+// -document 
+
+//(state)
+// - wordbank
+var wordBank = ["function", "javascript", "document"]
+
+// - the current word 
+
+
+
+//(functions)
+function startGame() {
+    console.log("The game is starting.")
+    // - start game 
+    // timer reset 
+    startTimer();
+    console.log("game is starting")
+    //pick a word form the wordbank
+    // generate an unguessed word
+    // render the unguessed word 
+}
+
+function startTimer () {
+
+}
+
+function pickRandomWord() { 
+
+}
+// - guess letter 
+
+
+
+
+//(user interaction)
+// - user clicks the button 
+// - start game 
+// - user presses a key 
+// - guess a letter (someLetter)
+
+
+//initializaton=======================
+
