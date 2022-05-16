be87d38a035db624ab3cdbd2608e6a9fa2f275e1
sdmnny <sdmnny@gmail.com>
2022-05-16 15:37:19 -0400

    finalized psuedocode

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,17 +1,23 @@
-// (Dependencies)
-// - need start button
+    // (Dependencies)
+    // - need start button
 var startbutton = document.querySelector("#start");
-// -document 
+    // blanks container 
+    //reset button 
+    //wins element 
+    //losses element
+
+
+
+
 
 //(state)
-// - wordbank
-var chosenWord = "";
-var numBlanks = 0;
+    // the curent word 
+    // guessed word (may contain blanks)
 
 
-// - the current word 
 
 
+    
 
 //(functions)
 function startGame() {
@@ -26,31 +32,67 @@ function startGame() {
 }
 
 function startTimer () {
+    console.log("starting the timer")
     var timeLeft = 10;
     var timerInterval = setInterval (function )
+    //set the starting time 
+    //start the timer running every second 
+    //render the update time on the page 
+    //checking for a win? true or false
+    //checking for a win 
+    //other stop the timer.. you lose
 
 }
 
 
 function checkWin() {
+console.log("checking for a wins")
+    //return: does the guessed word mathc the current word? true or false 
 
+}
 
+function youLose() {
+    //record the loss
+    //display a "you lose" message 
+}
+
+function youWin() {
+
+    //
 }
 
 
 function pickRandomWord() { 
+    // get a random index 
+    //get the word at that index from the wordbank
+    
 
 }
-// - guess letter 
+
+
+function generateGuessed(word) {
+    // turn the words into blanks return it 
+    // render the unguessed word 
+}
+
+function renderGuessedWord() {
+    //put the guessed word on the page with blanks 
+}
+
+function guessLetter (key) {
+    // check if the key is in the current word 
+    // yes? go thru the guessed word and replace blanks with that letter where appropriate
+}
 
 
 
 
 //(user interaction)
-// - user clicks the button 
-// - start game 
-// - user presses a key 
-// - guess a letter (someLetter)
+    // - user clicks the button 
+    // - start game 
+    // - user presses a key 
+    // - guess a letter (someLetter)
+    // a user presses reset scores 
 
 
 //initializaton=======================
